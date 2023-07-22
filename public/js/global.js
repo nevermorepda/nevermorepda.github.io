70ee69373ae98a4a800d59db30ff32de001affed
Kennet <nevermorepda@gmail.com>
2023-07-23 01:13:16 +0700

    develo0p

--- public/js/global.js
+++ public/js/global.js
@@ -9,6 +9,18 @@ $( document ).ready(function() {
             $(this).attr('show',0)
         }
     })
+    $('.listToggle h6').click(function() {
+        if($(this).attr('status') == 'off') {
+            $(this).find('i').addClass('fa-chevron-up')
+            $(this).find('i').removeClass('fa-chevron-down')
+            $(this).attr('status','on')
+        } else {
+            $(this).find('i').addClass('fa-chevron-down')
+            $(this).find('i').removeClass('fa-chevron-up')
+            $(this).attr('status','off')
+        }
+        $(this).parents('li').find('p').toggle('fast')
+    })
 });
 
 $(document).on('click','.videoBoxPC .fa-play', function() {
@@ -16,7 +28,7 @@ $(document).on('click','.videoBoxPC .fa-play', function() {
     $(this).addClass('fa-pause')
     document.getElementById('videoPC').play();
 })
-    $(document).on('click','.videoBoxPC .fa-pause', function() {
+$(document).on('click','.videoBoxPC .fa-pause', function() {
     $(this).removeClass('fa-pause')
     $(this).addClass('fa-play')
     document.getElementById('videoPC').pause();
@@ -32,3 +44,4 @@ $(document).on('click','.bannerMobile .fa-play', function() {
     $(this).addClass('fa-play')
     document.getElementById('videoMobile').pause();
 })
+
