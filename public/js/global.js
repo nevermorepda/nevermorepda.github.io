a55083c5d4b772d1d0c79365e52916a9f11c0417
Kennet <nevermorepda@gmail.com>
2023-07-28 01:51:20 +0700

    develop

--- public/js/global.js
+++ public/js/global.js
@@ -52,6 +52,22 @@ $( document ).ready(function() {
     $('.showLoginPage').click(function() {
         $('.signInPage').show()
     })
+    $('.showPopup').click(function() {
+        $('.popup').show()
+    })
+    $('.closePopup').click(function() {
+        $('.popup').hide()
+    })
+    $('.orderBox .edit').click(function() {
+        $(this).parents('.updating').find('input').removeAttr('disabled')
+        $(this).parents('.updating').find('select').removeAttr('disabled')
+        $(this).parents('.updating').find('.btnBoxEdit').show()
+    })
+    $('.orderBox .btnBoxEdit .btnCancel').click(function() {
+        $(this).parents('.updating').find('input').attr('disabled','true')
+        $(this).parents('.updating').find('select').attr('disabled','true')
+        $(this).parents('.btnBoxEdit').hide()
+    })
 });
 
 $(document).on('click','.videoBoxPC .fa-play', function() {
